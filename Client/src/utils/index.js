e0c6901d310df5a89346e1f8b5108715e47666df
Vinod Chandra <vinodchandra979@gmail.com>
2024-09-02 00:08:58 +0530

    env file not read

--- Client/src/utils/index.js
+++ Client/src/utils/index.js
@@ -1,3 +1,6 @@
 export const baseUrl = import.meta.env.VITE_API_URL  === 'production'
 ? 'https://myportfoliosite-02yh.onrender.com'
 : 'http://localhost:3000';
+
+
+console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
